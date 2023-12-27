d1ce6000f6a99f7248a62bed92524414913c6718
Taras <taras.sholtys@gmail.com>
2023-12-27 20:01:12 +0200

    added nav links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex m-2">
+          <div className="block m-2">
+            <Link href={`/posts`} className="text-blue-500 font-medium hover:underline active:underline">Posts list</Link>
+          </div>
+          <div className="block m-2">
+            <Link href={`/`} className="text-blue-500 font-medium hover:underline active:underline">Map</Link>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
