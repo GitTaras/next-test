a9e91aa41bf12923f2f7eaccde5b4f6542af19c7
Taras <taras.sholtys@gmail.com>
2023-12-27 21:54:41 +0200

    boring with env

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import MapboxMap from "./components/mapbox";
 
 const testenv = process.env.NEXT_PUBLIC_TEST_ENV;
 const testenv1 = process.env.TEST_ENV;
+const vercelurl = process.env.VERCEL_URL;
+const public_vercel = process.env.NEXT_PUBLIC_VERCEL_URL;
+
 
 export default function Home() {
   return (
@@ -9,6 +12,8 @@ export default function Home() {
       <MapboxMap/>
       <div>public Test env is: {testenv}</div>
       <div>private Test env is: {testenv1}</div>
+      <div>public vercel url is: {public_vercel}</div>
+      <div>private vercel url is: {vercelurl}</div>
     </div>
   )
 }
