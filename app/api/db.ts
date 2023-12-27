4d23aa6b0ed688ee8aeaa0bed49983b45e93ad86
Taras <taras.sholtys@gmail.com>
2023-12-27 19:43:04 +0200

    added connection string for db

--- app/api/db.ts
+++ app/api/db.ts
@@ -1,11 +1,18 @@
-import { Pool } from "pg";
+import {Pool, ClientConfig} from "pg";
 
-const conn = new Pool({
-    user: process.env.PGSQL_USER,
-    password: process.env.PGSQL_PASSWORD,
-    host: process.env.PGSQL_HOST,
-    port: Number(process.env.PGSQL_PORT),
-    database: process.env.PGSQL_DATABASE,
-});
+let params: ClientConfig = {};
+
+if (process.env.PGSQL_STRING) {
+    params.connectionString = process.env.PGSQL_STRING;
+} else {
+    params.user = process.env.PGSQL_USER;
+    params.password = process.env.PGSQL_PASSWORD;
+    params.host = process.env.PGSQL_HOST;
+    params.port = Number(process.env.PGSQL_PORT);
+    params.database = process.env.PGSQL_DATABASE
+}
+
+
+const conn = new Pool(params);
 
 export default conn;
